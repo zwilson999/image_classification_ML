6d4a7b7241f637798b32c9ccf6771f75ae7c1efd
zwilson999 <zwilson994@yahoo.com>
2020-06-27 21:43:33 -0400

    Added updates and fixed some indexing issues

--- src/cifar_10_CNN_train.py
+++ src/cifar_10_CNN_train.py
@@ -121,5 +121,3 @@ if __name__ == "__main__":
     """
     training below here
     """
-
-
--- src/tSNE_image.py
+++ src/tSNE_image.py
@@ -1,16 +1,3 @@
-# import numpy as np # linear algebra
-# import os
-# import matplotlib.pyplot as plt
-# import argparse
-
-# from sklearn.manifold import TSNE
-# from sklearn.preprocessing import StandardScaler
-# from time import time
-# from PIL import Image
-# from keras.datasets import cifar10
-# from keras.utils import to_categorical
-
- 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
@@ -25,14 +12,7 @@ from pathlib import Path
 
 
 
-def load_label_names():
-    """
-    Provide Label names
-    """
-    return ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
-
-def load_CIFAR_batch(file_name: Path):
+def load_CIFAR_batch(file_name: Path) -> np.ndarray:
     """ load single batch of cifar """
     with open(file_name, 'rb') as f:
         datadict = pickle.load(f, encoding = 'bytes')
@@ -43,7 +23,7 @@ def load_CIFAR_batch(file_name: Path):
 
         return X, Y
 
-def load_CIFAR10(root_path):
+def load_CIFAR10(root_path) -> np.ndarray:
     """
     load all of cifar and process the data batches into the training set, and the test_batch into the test set.
     later will sample these to provide a dev set as well.
@@ -67,21 +47,19 @@ def load_CIFAR10(root_path):
     X_test, Y_test = load_CIFAR_batch(os.path.join(root_path, 'test_batch'))
     return X_train, Y_train, X_test, Y_test
 
-def get_CIFAR10_data(num_training = 49000, num_validation = 1000, num_test = 10000):
+def get_CIFAR10_data():
     """
-    total data is 60000 images, 
+    total data is 60000 images
     """
     # Load the raw CIFAR-10 data
     cifar10_dir = '../data/raw/cifar-10-batches-py/'
     X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 
     # normalize the xtrain and xtest data
-    x_train = X_train.astype('float32')
-    x_test = X_test.astype('float32')
-
-    x_train /= 255.0
-    x_test /= 255.0
-
+    # X_train = X_train.astype('float32')
+    # X_test = X_test.astype('float32')
+    # X_train /= 255.0
+    # X_test /= 255.0
 
     return X_train, y_train, X_test, y_test
 
@@ -105,40 +83,60 @@ def pandify(xtrain: np.ndarray, ytrain: np.ndarray, xtest:np.ndarray, ytest: np.
     return xtrain_df, ytrain_df, xtest_df, ytest_df
 
 def create_master_data(xtrain_df: pd.DataFrame, ytrain_df: pd.DataFrame, xtest_df: pd.DataFrame, ytest_df: pd.DataFrame) -> pd.DataFrame:
+
+
     # combine all data frames into master
     train_frames = [xtrain_df, ytrain_df]
-    train_df = pd.concat(train_frames, axis = 1, sort = False)
-    print(train_df.shape)
+    train_df = pd.concat(train_frames, axis = 1, sort = False) # like an outer join (left to right)
+    #print(train_df.iloc[[0,25689]])
 
     test_frames = [x_test_df, y_test_df]
-    test_df = pd.concat(test_frames, axis = 1, sort = False)
-    print(test_df.shape)
+    test_df = pd.concat(test_frames, axis = 1, sort = False) # like an outer join (left to right)
+    #print(test_df.iloc[[0,-1]])
 
-
+    labels_dict = {0: 'airplane',
+                   1: 'automobile',
+                   2: 'bird',
+                   3: 'cat',
+                   4: 'deer',
+                   5: 'dog',
+                   6: 'frog',
+                   7: 'horse',
+                   8: 'ship',
+                   9: 'truck'
+    }
     # master frame of all data
     all_frames = [train_df, test_df]
-    master_df = pd.concat(all_frames, sort = False)
-    print(master_df.shape)
-
+    master_df = pd.concat(all_frames, sort = False).reset_index(drop = True) # join all frames together row-wise and reset the index
+    master_df.label = [labels_dict[item] for item in master_df.label] # convert label integers to their proper string label
+    #print(master_df.tail())
+    #print(master_df.shape)
     return master_df
 
 def tSNE_image(master_data: pd.DataFrame, n_rows_selected: int, n_iterations: int, perplexities: list, learning_rate: float, 
                plots_output_path: Path, n_components: int = 2):
 
-    df_new = master_data[:n_rows_selected]
-    label = df_new.label
-    #look at distribution of selected rows of data
-    crosstab = pd.crosstab(index = df_new['label'], columns='count')
+    X = master_data[:n_rows_selected] # select a subset of data
+    label = X.label
+
+    # look at distribution of selected rows of data
+    crosstab = pd.crosstab(index = X['label'], columns = 'count')
     print(crosstab)
+
+    # drop label column (it isnt a feature) and cast the df to numpy ndarray for t-SNE algorithm
+    X = X.drop(columns = ['label'], axis = 1).to_numpy()
+    # use standard scaler to scale data with mean = 0 and std deviation 1
+    X = StandardScaler().fit_transform(X) 
+
 
     #produce plots
     for perplexity in perplexities:
-        X = df_new
-        t0 = time()
+        
+        time_start = time()
         tsne = TSNE(n_components = n_components, init = 'random', random_state = 0, perplexity = perplexity, learning_rate = learning_rate)
         y = tsne.fit_transform(X)
-        t1 = time()
-        print("Perplexity = %d completed in %.2g sec" % (perplexity, t1 - t0))
+        time_end = time()
+        print("Perplexity = %d completed in %.2g sec" % (perplexity, time_end - time_start))
 
         reduced_df = np.vstack((y.T, label)).T
         reduced_df = pd.DataFrame(data = reduced_df, columns = ["Dim1", "Dim2", "label"])
@@ -148,7 +146,8 @@ def tSNE_image(master_data: pd.DataFrame, n_rows_selected: int, n_iterations: in
     
         filename = "n%d_perplex%d_plot.png" % (n_rows_selected, perplexity)
         fullpath = plots_output_path.joinpath(filename)
-    
+
+        g.fig.suptitle("tSNE with %d rows and perplexity = %d" % (n_rows_selected, perplexity))
         g.savefig(fullpath)
 
 if __name__ == "__main__":
@@ -174,12 +173,9 @@ if __name__ == "__main__":
     master_data_df = create_master_data(x_train_df, y_train_df, x_test_df, y_test_df)
 
     perplexities = [5, 30, 50, 100]
+
+    #the filepath where you want to save plots to on your local machine
     plot_output_path = Path("../data/processed/tSNE_plots").resolve()
 
     # run tSNE and save plots to output path
-    tSNE_image(master_data_df, 10000, 50000, perplexities, 200, 2)
-
-
-    #the filepath where you want to save plots to on your local machine
-
-
+    tSNE_image(master_data_df, 100, 4000, perplexities, 200, plot_output_path, 2)
